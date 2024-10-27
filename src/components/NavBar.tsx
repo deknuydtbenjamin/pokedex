d9d849657109e57b707e3c31834abe39432a0e0a
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-10-27 14:31:45 +0100

    first commit rendering-list.1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,24 +8,19 @@ type NavBarProps = {
 	setPokemonIndex: (index: number) => void;
 	pokemonList: Pokemon[];
 };
-function NavBarProps({
-	pokemonIndex,
-	setPokemonIndex,
-	pokemonList,
-}: NavBarProps) {
+function NavBar({ setPokemonIndex, pokemonList }: NavBarProps) {
 	return (
 		<div>
-			{pokemonIndex > 0 && (
-				<button type="button" onClick={() => setPokemonIndex(pokemonIndex - 1)}>
-					Précédent
+			{pokemonList.map((pokemon, pokemonIndex) => (
+				<button
+					type="button"
+					key={pokemon.name}
+					onClick={() => setPokemonIndex(pokemonIndex)}
+				>
+					{pokemon.name}
 				</button>
-			)}
-			{pokemonIndex < pokemonList.length - 1 && (
-				<button type="button" onClick={() => setPokemonIndex(pokemonIndex + 1)}>
-					Suivant
-				</button>
-			)}
+			))}
 		</div>
 	);
 }
-export default NavBarProps;
+export default NavBar;
